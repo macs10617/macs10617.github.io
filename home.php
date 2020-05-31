9860e7132bb133dbb6b23e6ec6bf946dfac7ad07
Andrew Origin <macs10617>
2020-05-31 15:54:38 +0300

    pay

---
+++ home.php
@@ -0,0 +1,10 @@
+<? top ('darova') ?>
+
+<div class="padding">
+<form action ="/form" method "post">
+<p><input type="text" name "sum" placeholder="Summa"></p>
+<p><input type="submit" name "enter" placeholder="Отправить"></p>
+</form>
+</div>
+
+<? bottom() ?>
